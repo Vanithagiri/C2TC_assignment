0c6cba5e5f4989eebdff4c53823436723893a413
Vanithagiri <vanithagiri05@gmail.com>
2025-08-19 21:54:57 +0530

    Uploaded assignment 3

---
+++ C2TC_java_assignment/src/assignment3/Item.java
@@ -0,0 +1,23 @@
+package assignment3;
+
+public class Item {
+    private int id;
+    private String title;
+
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void displayInfo() {
+        System.out.println("Item [ID: " + id + ", Title: " + title + "]");
+    }
+}
